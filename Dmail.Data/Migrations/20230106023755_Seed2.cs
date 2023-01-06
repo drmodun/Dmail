e3fe01046bf65a2c68d3d937981c5b8c47dfaece
drmodun <jan.modun.st@gmail.com>
2023-01-06 03:43:42 +0100

    Started seeding

---
+++ Dmail.Data/Migrations/20230106023755_Seed2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dmail.Data.Migrations
+{
+    public partial class Seed2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "Body", "CreatedAt", "DateOfEvent", "IsEvent", "SenderId", "Title" },
+                values: new object[] { 1, "Pomoc pls", new DateTime(2020, 8, 31, 22, 0, 0, 0, DateTimeKind.Utc), new DateTime(2023, 1, 6, 2, 37, 54, 836, DateTimeKind.Utc).AddTicks(1887), false, 1, "Pomoc" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "_password" },
+                values: new object[,]
+                {
+                    { 3, "Marko@markovi.markic", "marko" },
+                    { 4, "Duje@dump.hr", "Kick" },
+                    { 5, "Janko@gmail.com", "janv1" },
+                    { 6, "bart@dump.hr", "bartV10" },
+                    { 7, "Mao@yahoo.com", "mao" },
+                    { 8, "Fake@fakeemail.fakecountry", "Fake" },
+                    { 9, "Empty@empty.empty", "Empty" },
+                    { 10, "User@adress.domain", "Password" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
--- Dmail.Data/Seeds/Seeder.cs
+++ Dmail.Data/Seeds/Seeder.cs
@@ -16,7 +16,27 @@ namespace Dmail.Data.Seeds
             {
                 new User("Jan@gmail.com", "janv2")
                 { Id = 1},
-                new User("bartol@dump.hr", "bartolV10"){Id= 2}
+                new User("bartol@dump.hr", "bartolV10"){Id= 2},
+                new User("Marko@markovi.markic", "marko"){Id= 3},
+                new User("Duje@dump.hr", "Kick"){Id= 4},
+                new User("Janko@gmail.com", "janv1")
+                { Id = 5},
+                new User("bart@dump.hr", "bartV10"){Id= 6},
+                new User("Mao@yahoo.com", "mao"){Id= 7},
+                new User("Fake@fakeemail.fakecountry", "Fake"){Id= 8},
+                new User("Empty@empty.empty", "Empty"){Id= 9},
+                new User("User@adress.domain", "Password") { Id =10}
+            });
+            modelBuilder.Entity<Message>().HasData(new List<Message>
+            {
+                new Message()
+                {
+                    Id = 1,
+                    SenderId=1,
+                    Title="Pomoc",
+                    Body="Pomoc pls",
+                    CreatedAt=new DateTime(2020, 9, 1).ToUniversalTime()
+                }
             });
         }
     }
